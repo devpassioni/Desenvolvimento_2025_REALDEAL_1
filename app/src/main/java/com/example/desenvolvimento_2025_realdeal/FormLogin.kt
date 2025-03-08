0eb043feb8b0d3119f319567354be64182ffc2c8
Passioni <jhow-rock@live.com>
2025-03-08 19:47:11 -0300

    Front end funcionando *Falta mudar elementos pra centralizar

--- app/src/main/java/com/example/desenvolvimento_2025_realdeal/FormLogin.kt
+++ app/src/main/java/com/example/desenvolvimento_2025_realdeal/FormLogin.kt
@@ -1,12 +1,19 @@
 package com.example.desenvolvimento_2025_realdeal
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView  // Importando TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 public class FormLogin : AppCompatActivity() {
+
+    private lateinit var botao_cadastro_login: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,7 +22,22 @@ public class FormLogin : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+            }
+        iniciar_componentes()
+        supportActionBar?.hide()
+
     }
+        private fun iniciar_componentes() {
+            botao_cadastro_login = findViewById(R.id.botao_cadastro_login)
+
+            botao_cadastro_login.setOnClickListener {
+                // Cria o Intent para abrir a Activity de cadastro
+                val intent = Intent(this, FormCadastro::class.java)
+                startActivity(intent)
+            }
+        }
+
 }
+
+
 
